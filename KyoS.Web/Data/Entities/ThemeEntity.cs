f50ddc2cd726938b064bca790906806df274b057
Oscar Hernández Baute <elpuya84@gmail.com>
2022-02-10 18:38:06 -0500

    HotFix01
    1- Deleted restriction of maxsize of ThemeEntity table column "Name"
    2- Updated Create3 method of ThemeController

--- KyoS.Web/Data/Entities/ThemeEntity.cs
+++ KyoS.Web/Data/Entities/ThemeEntity.cs
@@ -8,7 +8,6 @@ namespace KyoS.Web.Data.Entities
         public int Id { get; set; }
 
         [Display(Name = "Topic")]
-        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]        
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string Name { get; set; }
 
