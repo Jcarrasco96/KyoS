0002d67600dc1f824600205ee79e84c22e980f59
yoanky madrazo <ymadrazovalladares@gmail.com>
2022-04-06 12:37:07 -0400

    Merge Develop in T_124

--- KyoS.Web/Migrations/20220405174404_add_migration_hotfix8_1.Designer.cs
+++ KyoS.Web/Migrations/20220405174404_add_migration_hotfix8_1.Designer.cs
@@ -4,14 +4,16 @@ using KyoS.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KyoS.Web.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220405174404_add_migration_hotfix8_1")]
+    partial class add_migration_hotfix8_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
