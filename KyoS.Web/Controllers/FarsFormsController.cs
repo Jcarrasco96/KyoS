8357404aec8c40aa1c568d132f6f061f0e7ff6b9
yoanky madrazo <ymadrazovalladares@gmail.com>
2022-05-13 22:40:51 -0400

    CREATE VIEW CLIENT WITHOUT FARS

--- KyoS.Web/Controllers/FarsFormsController.cs
+++ KyoS.Web/Controllers/FarsFormsController.cs
@@ -364,5 +364,28 @@ namespace KyoS.Web.Controllers
 
             return null;
         }
+
+        public async Task<IActionResult> ClientswithoutFARS(int idError = 0)
+        {
+            UserEntity user_logged = await _context.Users
+
+                                                   .Include(u => u.Clinic)
+                                                   .ThenInclude(c => c.Setting)
+
+                                                   .FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+
+            if (user_logged.Clinic == null || user_logged.Clinic.Setting == null || !user_logged.Clinic.Setting.MentalHealthClinic)
+            {
+                return RedirectToAction("NotAuthorized", "Account");
+            }
+
+            List<ClientEntity> ClientList = await _context.Clients
+                                                          .Include(n => n.FarsFormList)
+                                                          .Where(n => n.FarsFormList.Count == 0 && n.Clinic.Id == user_logged.Clinic.Id)
+                                                          .ToListAsync();
+
+            return View(ClientList);
+
+        }
     }
 }
