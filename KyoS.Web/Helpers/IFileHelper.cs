840ebba46e0c43da5234dc79ffd9c78557b14763
Oscar Hernández Baute <elpuya84@gmail.com>
2023-04-15 14:39:15 -0400

    Reports of Community Health Therapy Center Clinic. Part #3

---
+++ KyoS.Web/Helpers/IFileHelper.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace KyoS.Web.Helpers
+{
+    public interface IFileHelper
+    {
+        public byte[] Zip(List<FileContentResult> fileContentList);        
+    }
+}
