5f983ab5c83098df477dfc40d8332bb0d305a93d
yoanky madrazo <ymadrazovalladares@gmail.com>
2023-07-25 18:53:34 -0400

    migration with code 90791 for bio and brief

---
+++ KyoS.Web/Migrations/20230725225246_CODE-90791.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KyoS.Web.Migrations
+{
+    public partial class CODE90791 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Code90791",
+                table: "Brief",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Code90791",
+                table: "Bio",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code90791",
+                table: "Brief");
+
+            migrationBuilder.DropColumn(
+                name: "Code90791",
+                table: "Bio");
+        }
+    }
+}
