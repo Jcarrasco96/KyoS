e149c0f3c806b1693e86c07802dc885f8893457c
yoanky madrazo <ymadrazovalladares@gmail.com>
2023-07-27 15:02:14 -0400

    improves referred export excell v1.1

--- KyoS.Web/Helpers/ExportExcellHelper.cs
+++ KyoS.Web/Helpers/ExportExcellHelper.cs
@@ -940,91 +940,95 @@ namespace KyoS.Web.Helpers
         public byte[] ExportAllReferreds(List<Client_Referred> clients)
         {
             var Clients = clients;
-
-            using (var workbook = new XLWorkbook())
+            var workbook = new XLWorkbook();
+            if (clients.Count() > 0)
             {
-                var worksheet = workbook.Worksheets.Add("All Clients (" + clients.Count() + ")");
-                worksheet.Cells("A1").Value = "COMMUNITY HEALTH THERAPY CENTER. INC";
-                worksheet.Cell(2, 1).Value = clients.ElementAt(0).Client.Clinic.Name;
-                worksheet.Cell(3, 1).Value = "Date Report: " + DateTime.Today.ToLongDateString();
-                var currentRow = 4;
-                worksheet.Cell(currentRow, 1).Value = "Referred Name";
-                worksheet.Cell(currentRow, 2).Value = "Referred Address";
-                worksheet.Cell(currentRow, 3).Value = "Referred type";
-                worksheet.Cell(currentRow, 4).Value = "Client Name";
-                worksheet.Cell(currentRow, 5).Value = "Service";
-                worksheet.Cell(currentRow, 6).Value = "Code";
-                worksheet.Cell(currentRow, 7).Value = "Admission";
-                worksheet.Cell(currentRow, 8).Value = "Status";
-               
-
-
-                worksheet.Style.Font.Bold = true;
-                IXLRange range = worksheet.Range(worksheet.Cell(currentRow, 1).Address, worksheet.Cell(currentRow, 8).Address);
-                range.Style.Fill.SetBackgroundColor(XLColor.LightGray);
-                range.SetAutoFilter();
-
-                foreach (var item in Clients)
-                {
-                    currentRow++;
                 
-                    worksheet.Cell(currentRow, 1).Value = item.Referred.Name;
-                    worksheet.Cell(currentRow, 2).Value = item.Referred.Address;
-                    worksheet.Cell(currentRow, 3).Value = item.type;
-                    if (item.type == Common.Enums.ReferredType.In)
-                    {
-                        worksheet.Cell(currentRow, 3).Style.Fill.SetBackgroundColor(XLColor.Green);
-                    }
-                    else
-                    {
-                        worksheet.Cell(currentRow, 3).Style.Fill.SetBackgroundColor(XLColor.Gray);
-                    }
-                    worksheet.Cell(currentRow, 4).Value = item.Service;
-                    worksheet.Cell(currentRow, 5).Value = item.Client.Name;
-                    worksheet.Cell(currentRow, 6).Value = item.Client.Code;
-                    worksheet.Cell(currentRow, 7).Value = item.Client.AdmisionDate;
-                    worksheet.Cell(currentRow, 8).Value = item.Client.Status;
-                    if (item.Client.Status == Common.Enums.StatusType.Open)
-                    {
-                        worksheet.Cell(currentRow, 8).Style.Fill.SetBackgroundColor(XLColor.Green);
-                    }
-                    else
-                    {
-                        worksheet.Cell(currentRow, 8).Style.Fill.SetBackgroundColor(XLColor.Red);
-                    }
-                    
-                    IXLRange rangeCurrent = worksheet.Range(worksheet.Cell(currentRow, 1).Address, worksheet.Cell(currentRow, 8).Address);
-                    rangeCurrent.Style.Font.FontSize = 11;
-                    rangeCurrent.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-                    rangeCurrent.Style.Font.Bold = false;
-
-                }
-
-                worksheet.ColumnsUsed().AdjustToContents();
-                IXLRange range1 = worksheet.Range(worksheet.Cell(1, 1).Address, worksheet.Cell(1, 8).Address);
-                range1.Style.Font.FontSize = 18;
-                range1.Style.Font.Bold = false;
-                range1.Style.Alignment.Vertical = XLAlignmentVerticalValues.Top;
-                range1.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                range1.Merge();
-                IXLRange range2 = worksheet.Range(worksheet.Cell(2, 1).Address, worksheet.Cell(2, 8).Address);
-                range2.Style.Font.FontSize = 16;
-                range2.Style.Font.Bold = false;
-                range2.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                range2.Merge();
-                IXLRange range3 = worksheet.Range(worksheet.Cell(3, 1).Address, worksheet.Cell(3, 8).Address);
-                range3.Style.Font.FontSize = 14;
-                range3.Style.Font.Bold = false;
-                range3.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                range3.Merge();
-
-                using (var stream = new MemoryStream())
                 {
-                    workbook.SaveAs(stream);
-                    var content = ConvertStreamToByteArray(stream);
-                    //return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Facilitator.xlsx");
-                    return stream.ToArray();
+                    var worksheet = workbook.Worksheets.Add("All Clients (" + clients.Count() + ")");
+                    worksheet.Cells("A1").Value = "COMMUNITY HEALTH THERAPY CENTER. INC";
+                    worksheet.Cell(2, 1).Value = clients.ElementAt(0).Client.Clinic.Name;
+                    worksheet.Cell(3, 1).Value = "Date Report: " + DateTime.Today.ToLongDateString();
+                    var currentRow = 4;
+                    worksheet.Cell(currentRow, 1).Value = "Referred Name";
+                    worksheet.Cell(currentRow, 2).Value = "Referred Address";
+                    worksheet.Cell(currentRow, 3).Value = "Referred type";
+                    worksheet.Cell(currentRow, 4).Value = "Client Name";
+                    worksheet.Cell(currentRow, 5).Value = "Service";
+                    worksheet.Cell(currentRow, 6).Value = "Code";
+                    worksheet.Cell(currentRow, 7).Value = "Admission";
+                    worksheet.Cell(currentRow, 8).Value = "Status";
+
+
+
+                    worksheet.Style.Font.Bold = true;
+                    IXLRange range = worksheet.Range(worksheet.Cell(currentRow, 1).Address, worksheet.Cell(currentRow, 8).Address);
+                    range.Style.Fill.SetBackgroundColor(XLColor.LightGray);
+                    range.SetAutoFilter();
+
+                    foreach (var item in Clients)
+                    {
+                        currentRow++;
+
+                        worksheet.Cell(currentRow, 1).Value = item.Referred.Name;
+                        worksheet.Cell(currentRow, 2).Value = item.Referred.Address;
+                        worksheet.Cell(currentRow, 3).Value = item.type;
+                        if (item.type == Common.Enums.ReferredType.In)
+                        {
+                            worksheet.Cell(currentRow, 3).Style.Fill.SetBackgroundColor(XLColor.Green);
+                        }
+                        else
+                        {
+                            worksheet.Cell(currentRow, 3).Style.Fill.SetBackgroundColor(XLColor.Gray);
+                        }
+                        worksheet.Cell(currentRow, 4).Value = item.Service;
+                        worksheet.Cell(currentRow, 5).Value = item.Client.Name;
+                        worksheet.Cell(currentRow, 6).Value = item.Client.Code;
+                        worksheet.Cell(currentRow, 7).Value = item.Client.AdmisionDate;
+                        worksheet.Cell(currentRow, 8).Value = item.Client.Status;
+                        if (item.Client.Status == Common.Enums.StatusType.Open)
+                        {
+                            worksheet.Cell(currentRow, 8).Style.Fill.SetBackgroundColor(XLColor.Green);
+                        }
+                        else
+                        {
+                            worksheet.Cell(currentRow, 8).Style.Fill.SetBackgroundColor(XLColor.Red);
+                        }
+
+                        IXLRange rangeCurrent = worksheet.Range(worksheet.Cell(currentRow, 1).Address, worksheet.Cell(currentRow, 8).Address);
+                        rangeCurrent.Style.Font.FontSize = 11;
+                        rangeCurrent.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                        rangeCurrent.Style.Font.Bold = false;
+
+                    }
+
+                    worksheet.ColumnsUsed().AdjustToContents();
+                    IXLRange range1 = worksheet.Range(worksheet.Cell(1, 1).Address, worksheet.Cell(1, 8).Address);
+                    range1.Style.Font.FontSize = 18;
+                    range1.Style.Font.Bold = false;
+                    range1.Style.Alignment.Vertical = XLAlignmentVerticalValues.Top;
+                    range1.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    range1.Merge();
+                    IXLRange range2 = worksheet.Range(worksheet.Cell(2, 1).Address, worksheet.Cell(2, 8).Address);
+                    range2.Style.Font.FontSize = 16;
+                    range2.Style.Font.Bold = false;
+                    range2.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    range2.Merge();
+                    IXLRange range3 = worksheet.Range(worksheet.Cell(3, 1).Address, worksheet.Cell(3, 8).Address);
+                    range3.Style.Font.FontSize = 14;
+                    range3.Style.Font.Bold = false;
+                    range3.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    range3.Merge();
+
+                    
                 }
+            }
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                var content = ConvertStreamToByteArray(stream);
+                //return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Facilitator.xlsx");
+                return stream.ToArray();
             }
         }
 
